c93c47f45bf565dbb6a315c4f86eb30b5e743b06
0xjoshva <joshuasteed123@gmail.com>
2022-11-01 15:07:49 +0200

    good

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,14 +15,23 @@
           :class="{ unread: !noti.messageRead }"
         >
           <div class="content">
+            <div class="row">
+              <img :src="noti.profilePic" alt="" class="profilepic" />
 
-            <div class="alert">
-              <p class="name">{{ noti.fullName }}</p>
-              <p class="action">{{ noti.userAction }}</p>
-              <p class="event">{{ noti.event }}</p>
+              <div class="alert">
+                <p class="name">{{ noti.fullName }}</p>
+                <p class="action">{{ noti.userAction }}</p>
+                <p class="event" :class="{ blue: noti.event === 'Chess Club' }">
+                  {{ noti.event }}
+                </p>
+              </div>
             </div>
-            <p class="time">{{ noti.time }}</p>
+ <p class="time">{{ noti.time }}</p>
+          <div class="private-message">
+            <p>{{ noti.privateMessage }}</p>
           </div>
+          </div>
+         
         </div>
       </div>
     </div>
@@ -35,36 +44,40 @@ export default {
     return {
       notis: [
         {
-          fullName: "Mark Webber",
-          profilePic: "url",
-          userAction: "reacted to your recent post",
+          fullName: "Mark Webber ",
+          profilePic:
+            "https://raw.githubusercontent.com/0xjoshva/notification-page/main/src/assets/avatar-mark-webber.webp",
+          userAction: "reacted to your recent post ",
           event: "My first tournament today",
           time: "1m ago",
           messageRead: false,
           privateMessage: "",
         },
         {
-          fullName: "Angela Grey",
-          profilePic: "url",
-          userAction: "followed you",
+          fullName: "Angela Grey ",
+          profilePic:
+            "https://raw.githubusercontent.com/0xjoshva/notification-page/main/src/assets/avatar-angela-gray.webp",
+          userAction: "followed you ",
           event: "",
           time: "5m ago",
           messageRead: false,
           privateMessage: "",
         },
         {
-          fullName: "Jacob Thompson",
-          profilePic: "url",
-          userAction: "has joined your group",
+          fullName: "Jacob Thompson ",
+          profilePic:
+            "https://raw.githubusercontent.com/0xjoshva/notification-page/main/src/assets/avatar-jacob-thompson.webp",
+          userAction: "has joined your group ",
           event: "Chess Club",
           time: "1 day ago",
           messageRead: false,
           privateMessage: "",
         },
         {
-          fullName: "Rizky Hasanuddin",
-          profilePic: "url",
-          userAction: "sent you a private message",
+          fullName: "Rizky Hasanuddin ",
+          profilePic:
+            "https://raw.githubusercontent.com/0xjoshva/notification-page/main/src/assets/avatar-rizky-hasanuddin.webp",
+          userAction: "sent you a private message ",
           event: "",
           time: "5 days ago",
           messageRead: true,
@@ -72,27 +85,30 @@ export default {
             "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now ",
         },
         {
-          fullName: "Kimberly Smith",
-          profilePic: "url",
-          userAction: "commented on your picture",
+          fullName: "Kimberly Smith ",
+          profilePic:
+            "https://raw.githubusercontent.com/0xjoshva/notification-page/main/src/assets/avatar-kimberly-smith.webp",
+          userAction: "commented on your picture ",
           event: "",
           time: "1 week ago",
           messageRead: true,
           privateMessage: "",
         },
         {
-          fullName: "Nathan Peterson",
-          profilePic: "url",
-          userAction: "reacted to your recent post",
+          fullName: "Nathan Peterson ",
+          profilePic:
+            "https://raw.githubusercontent.com/0xjoshva/notification-page/main/src/assets/avatar-nathan-peterson.webp",
+          userAction: "reacted to your recent post ",
           event: "5 end-game strategies to increase your win rate",
           time: "2 weeks ago",
           messageRead: true,
           privateMessage: "",
         },
         {
-          fullName: "Anna Kim",
-          profilePic: "url",
-          userAction: "left the group",
+          fullName: "Anna Kim ",
+          profilePic:
+            "https://raw.githubusercontent.com/0xjoshva/notification-page/main/src/assets/avatar-anna-kim.webp",
+          userAction: "left the group ",
           event: "Chess Club",
           time: "2 weeks ago",
           messageRead: true,
@@ -118,7 +134,8 @@ section {
   display: flex;
   flex-direction: column;
   width: 50rem;
-  height: 50rem;
+  height: fit-content;
+  min-height: 50rem;
   background-color: var(--white);
   border-radius: 14px;
   padding-inline: 2rem;
@@ -133,27 +150,27 @@ section {
   height: fit-content;
   padding-bottom: 1rem;
 }
-.top h1{
-    font-weight: 800;
-    color: var(--vdblue);
+.top h1 {
+  font-weight: 800;
+  color: var(--vdblue);
 }
-.top h1 span{
-    font-weight: 800;
-    background: var(--blue);
-    color: var(--white);
-    font-size: .9rem;
-    padding-block: .15rem;
-    padding-inline: .55rem;
-    border-radius: 4px;
-    margin-left: .3rem;
-    vertical-align: middle;
+.top h1 span {
+  font-weight: 800;
+  background: var(--blue);
+  color: var(--white);
+  font-size: 0.9rem;
+  padding-block: 0.15rem;
+  padding-inline: 0.55rem;
+  border-radius: 4px;
+  margin-left: 0.3rem;
+  vertical-align: middle;
 }
-.readall-btn{
-    color: var(--dgblue);
-    background: none;
-    border: none;
-    outline: none;
-    font-weight: 500;
+.readall-btn {
+  color: var(--dgblue);
+  background: none;
+  border: none;
+  outline: none;
+  font-weight: 500;
 }
 .message-wrapper {
   display: flex;
@@ -172,11 +189,18 @@ section {
 .content {
   display: flex;
   flex-direction: column;
+  column-gap: 1rem;
+}
+.row {
+  display: flex;
+  flex-direction: row;
 }
 .alert {
-  display: flex;
-  column-gap: 0.5rem;
-  height: fit-content;
+width:100%;
+}
+p{
+    display:inline;
+ word-wrap: break-word;
 }
 .name {
   color: var(--vdblue);
@@ -190,8 +214,15 @@ section {
   color: var(--dgblue);
   font-weight: 650;
 }
+.profilepic {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
 .unread {
   background: var(--vlgblue);
 }
-
+.blue {
+  color: var(--blue) !important;
+}
 </style>
